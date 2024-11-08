8688f10e63db1b16d8c2331b3a85226a9244fd19
EnricoGollner <enricogollner.tech@gmail.com>
2024-11-08 16:56:27 -0300

    starting project

---
+++ lib/components/my_game.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+import 'package:color_switch_game/components/player.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+
+class MyGame extends FlameGame with TapCallbacks {
+  late Player myPlayer;
+
+  MyGame()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: 600,
+            height: 1000,
+          ),
+        );
+
+  @override
+  Color backgroundColor() => const Color(0xff222222);
+
+  @override
+  void onMount() {
+    world.add(myPlayer = Player());
+    super.onMount();
+  }
+
+  @override
+  void update(double dt) {
+    final double cameraY = camera.viewfinder.position.y;
+    final double playerY = myPlayer.position.y;
+
+    if (playerY < cameraY) camera.viewfinder.position = Vector2(0, playerY);
+    super.update(dt);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    myPlayer.jump();
+    super.onTapDown(event);
+  }
+}
